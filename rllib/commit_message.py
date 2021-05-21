f186c2ce4a7ba47a2f96dedaaca2ba86137919a2
Ugurkan Ates <ugurkanates97@gmail.com>
2021-05-21 19:58:27 +0300

    some main settings,notebook added

---
+++ rllib/commit_message.py
@@ -0,0 +1,23 @@
+from git import Repo
+import os
+import json
+def intro_repository(config,mode="Train"):
+
+ # Get git info to put in the config_*.txt file
+    repo = Repo(os.getcwd())
+    repo_name = repo.remotes.origin.url.split('.git')[0].split('/')[-1]
+    branch_name = repo.head.reference
+    commit = branch_name.commit
+    commit_small = str(commit)[0:8]
+
+    config_str = f"""
+      -------------- Repository information --------------
+      Repo name : {repo_name}
+      Branch name : {branch_name}
+      Commit : {commit} ({commit_small})
+      ----------------------------------------------------
+      Environment Related Config
+      {json.dumps(config, indent=8, default=str)}
+      {mode} Related Config
+      """
+    return config_str
--- rllib/main.py
+++ rllib/main.py
@@ -1,9 +1,30 @@
 import ray
 from ray import tune
+from ray.tune import CLIReporter
+
 from config.config_handler import getConfig
+from commit_message import intro_repository
+
 import argparse
 import torch
 import os
+import numpy as np
+
+SEED_VALUE = 5
+TOTAL_STEP = 1000 # total steps 
+TOTAL_TRAIN_ITER = 1000000 # how many times network updated
+
+def make_deterministic(seed):
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    # see https://github.com/pytorch/pytorch/issues/47672
+    cuda_version = torch.version.cuda
+    if cuda_version is not None and float(torch.version.cuda) >= 10.2:
+        os.environ['CUBLAS_WORKSPACE_CONFIG'] = '4096:8'
+    else:
+        torch.set_deterministic(True)  #Not all Operations support this.
+    torch.backends.cudnn.deterministic = True #This is only for Convolution no problem
 
 def main():
     parser = argparse.ArgumentParser()
@@ -17,6 +38,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+    if torch.cuda.is_available():
+        print("## CUDA available")
+        print(f"Current device: {torch.cuda.current_device()}")
+        print(
+            f"Device name: {torch.cuda.get_device_name(torch.cuda.current_device())}")
+    else:
+        print("## CUDA not available")
 
     ray.init(local_mode=True)
 
@@ -34,13 +62,36 @@ if __name__ == "__main__":
     config["exploration_config"]["epsilon_timesteps"] = 5000
     config["env"] = "drone_env"
     config["num_gpus"] = 1 if torch.cuda.is_available() else 0
+    
+    # Determinism 
+    config['seed'] = SEED_VALUE
+    make_deterministic(SEED_VALUE)
+
+    # Stopping criteria
+
+    stopx = {"timesteps_total": TOTAL_STEP}
+            #"training_iteration":TOTAL_TRAIN_ITER }
+
+    # Load Checkpoints
+
+    # Load state from checkpoint.
+    # if(train_config['checkpoint']):
+    #     agent.restore(train_config['checkpoint_path'])
+    # if required latedr                        
+    # tune run -> trial_dirname_creator=trial_name_id, trial_name_creator=trial_name_id,
+
+     # Setup the stopping condition
 
     checkpoint_path = os.path.join(os.getcwd(), "checkpoints")
-
+    config_str = intro_repository(config)
+    print(config_str)
     results = tune.run(ALG, config=config,
-                            checkpoint_freq=1, 
+                            checkpoint_freq=5, 
                             max_failures = 5,
+                            log_to_file=["logs_out.txt", "logs_err.txt"],
                             checkpoint_at_end = True,
+                            progress_reporter=CLIReporter(metric_columns=["loss","date", "training_iteration", "timesteps_total"]),
                             local_dir=checkpoint_path,
-                            checkpoint_score_attr="min-validation_loss",
-                            keep_checkpoints_num=10)
+                            keep_checkpoints_num=10,
+                            stop=stopx)
+    ray.shutdown()
