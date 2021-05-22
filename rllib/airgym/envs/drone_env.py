bf1e20eeba2dbbb3df098d72c58f5f5839163a1d
Yusuf Taskiran <ytaskiran70@gmail.com>
2021-05-22 14:50:40 +0300

    bug fix

--- rllib/airgym/envs/drone_env.py
+++ rllib/airgym/envs/drone_env.py
@@ -42,7 +42,7 @@ class AirSimDroneEnv(gym.Env):
         self.total_step = 0
         self.total_check = 5
         self.current_final = False
-        self.starting_positions = ([293, -349, -2]) #, [-212, 7, -2], [23, -14, -2], [-216, -362, -2], [160, -66, -2])
+        self.starting_positions = [[293, -349, -2]] #, [-212, 7, -2], [23, -14, -2], [-216, -362, -2], [160, -66, -2])
         self.houses = ["SM_House_27", "SM_House_85", "SM_House_22", "SM_House_287", "SM_House_4333"]
         self._setup_flight()
 
@@ -55,7 +55,7 @@ class AirSimDroneEnv(gym.Env):
         self.drone.armDisarm(True)
         self.setGeoFenceCoords()
         self.current_final = False
-        self.starting_pos = self.starting_positions[random.randint(0, 4)]
+        self.starting_pos = self.starting_positions[random.randint(0, len(self.starting_positions)-1)]
         # teleport the drone
         pose = self.drone.simGetVehiclePose()
         pose.position.x_val = self.starting_pos[0]
